ff1f98cd70848e402c3db3cc1ee7f817f0fd15d0
Kaczmarczykk97 <kamil.kaczmarczykk@gmail.com>
2023-08-25 14:17:14 +0200

    posirion, read more btn, style

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -3,13 +3,19 @@ import { useState } from "react";
 import classes from "./HomePage.module.css";
 
 import FUT from "../imgs/FUT.png";
+import unknown_player from "../imgs/unknown_player.png";
+import unknown_country from "../imgs/unknown_country.png";
+import unknown_club from "../imgs/unknown_club.png";
 import footballClubs from "./football-clubs";
 
 function HomePage() {
   const [playerName, setPlayerName] = useState("?");
-  const [playerImg, setPlayerImg] = useState("?");
-  const [clubImg, setClubImg] = useState("?");
-  const [nationalityImg, setNationalityImg] = useState("?");
+  const [playerImg, setPlayerImg] = useState("");
+  const [clubImg, setClubImg] = useState("");
+  const [nationalityImg, setNationalityImg] = useState("");
+  const [playerPosition, setPlayerPosition] = useState("?");
+  const [playerInfo, setPlayerInfo] = useState("#");
+
   const [userInput, setUserInput] = useState("");
 
   const userInputHandler = (e) => {
@@ -26,6 +32,8 @@ function HomePage() {
     console.log(info.player[0]);
     setPlayerName(info.player[0].strPlayer);
     setPlayerImg(info.player[0].strCutout);
+    setPlayerPosition(info.player[0].strPosition);
+    setPlayerInfo(info.player[0].idPlayer);
 
     const clubName = info.player[0].strTeam;
     const nationality = info.player[0].strNationality.toLowerCase();
@@ -50,25 +58,44 @@ function HomePage() {
       <div className={classes.gameContainer}>
         <div className={classes.cardContainer}>
           <img src={FUT} alt="FUT card" className={classes.futImg} />
-          <img src={playerImg} alt="player img" className={classes.playImg} />
+          <img
+            src={playerImg ? playerImg : unknown_player}
+            alt="player img"
+            className={classes.playImg}
+          />
 
           <img
-            src={clubImg.src ? clubImg.src : ""}
+            src={clubImg.src ? clubImg.src : unknown_club}
             alt="club img"
             className={classes.clubImg}
           />
 
           <img
-            src={nationalityImg}
+            src={nationalityImg ? nationalityImg : unknown_country}
             alt="nationality img"
             className={classes.nationalityImg}
           />
           <h3 className={classes.headingH3}>{playerName}</h3>
+          <p className={classes.playerPosition}>
+            Position: {playerPosition === "?" ? "?" : playerPosition}
+          </p>
+          <button className={classes.readMoreBtn}>
+            <a
+              href={
+                playerInfo === "#"
+                  ? "#"
+                  : `https://www.thesportsdb.com/player/${playerInfo}`
+              }
+              target="_blank"
+              rel="noreferrer"
+            >
+              Read More
+            </a>
+          </button>
         </div>
         <div className={classes.answearsContainer}>
           <h2 className={classes.headingH2}>Ask Your Question:</h2>
           <form onSubmit={userInputHandler}>
-            <label htmlFor="playerFinder">FIND YOUR PLAYER:</label>
             <input
               id="playerFinder"
               onChange={(e) => setUserInput(e.target.value)}
